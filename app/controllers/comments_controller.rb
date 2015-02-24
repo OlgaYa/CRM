3762d9b3b5797cc7e693b68461715f184f3d4d7e
Yaroslav Zhabko <Yaroslav.Zhabko@gmail.com>
2015-02-24 17:28:46 +0200

    finish comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
     task = Task.find(params[:task_id])
     time = DateTime.current()
     comment = task.comments.create(user_id: current_user.id , body: params[:body], datetime: time)
-    resp = "<div class='comment'><span class='comment_time'>" + time.strftime("%e.%m %H:%M") +
+    resp = "<div class='comment " + comment.id.to_s +
+            "'><span class='comment_time'>" + time.strftime("%e.%m %H:%M") +
             " </span><span>" + current_user.first_name +
             " </span> <a class='pull-right' data-remote='true'"+
             " rel='nofollow' data-method='delete' href='"+ comment_path(comment) + 
