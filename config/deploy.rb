3fc4ef728e3591233a971f2b9a5907d2fafb5383
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-02-25 12:33:03 +0200

    Deploy Version 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,9 @@ lock '3.3.5'
 set :repo_url, 'https://github.com/Anastasiya09/CRM'
 set :deploy_via, :remote_cache 
 set :application, "crm"
+set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/secrets.yml}
+
  
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -15,23 +18,27 @@ set :keep_releases, 5
  
 namespace :deploy do
  
-# desc 'Restart application'
-# 	task :restart do
-# 		on roles(:app), in: :sequence, wait: 5 do
-# 		# Restarts Phusion Passenger
-# 		execute :touch, release_path.join('tmp/restart.txt')
-# 	end
-# end
- 
-after :publishing, :restart
- 
-after :restart, :clear_cache do
-	on roles(:web), in: :groups, limit: 3, wait: 10 do
-	# Here we can do anything such as:
-	# within release_path do
-	# execute :rake, 'cache:clear'
+	# desc 'Restart application'
+	# 	task :restart do
+	# 		on roles(:app), in: :sequence, wait: 5 do
+	# 		# Restarts Phusion Passenger
+	# 		execute :touch, release_path.join('tmp/restart.txt')
+	# 	end
 	# end
+	 
+	after :publishing, :restart
+	 
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+		# Here we can do anything such as:
+		# within release_path do
+		# execute :rake, 'cache:clear'
+		# end
+		end
+	end
+	desc "Symlink shared config files"
+	task :symlink_config_files do
+	    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
 	end
 end
  
-end 
