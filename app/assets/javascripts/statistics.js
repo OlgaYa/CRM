091fcf0a08db0ecfbfac042631de776d582d6f8b
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-03-04 12:16:03 +0200

    Statistics

---
+++ app/assets/javascripts/statistics.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  function showOrHide(cb, cat) {
+    cb = document.getElementById(cb);
+    cat = document.getElementById(cat);
+    if (cb.checked) cat.style.display = "block";
+    else cat.style.display = "none";
+  }
+});
