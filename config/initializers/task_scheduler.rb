698fe63c2628f588a1a3e66b4e24d954a1f720aa
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-03-05 17:00:02 +0200

    recive messages

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,16 +1,16 @@
 require 'rubygems'
 require 'rufus/scheduler' 
 
- scheduler = Rufus::Scheduler.start_new
+scheduler = Rufus::Scheduler.start_new
 
- scheduler.cron '0 11 * * 1-5' do
+scheduler.cron '0 11 * * 1-5' do
  # every day of the week at 11:00 
  	User.reminder
- end 
+end 
 
- scheduler.in '30s' do
+scheduler.every '30s' do
  	Message.check_mailing
- end
+end
 
 
  
