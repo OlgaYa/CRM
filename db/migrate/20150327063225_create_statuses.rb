924c16ce6c31e743c8b9a15d0beb6d1aa636e24c
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-03-29 23:27:51 +0300

    statuses

--- db/migrate/20150327063225_create_statuses.rb
+++ db/migrate/20150327063225_create_statuses.rb
@@ -7,13 +7,4 @@ class CreateStatuses < ActiveRecord::Migration
     end
     add_column :tasks, :status_id, :integer
   end
-
-  # def save_status
-  #   status_names = Task.all.pluck(:status)
-  #   status_names.each{|name| Status.new(name: name).save }
-    # Task.all.each do |task|
-    #   status_id = Status.find_by(name: task.status).id
-    #   task.update_attribute(:status_id, status_id)
-    # end
-  # end
 end
