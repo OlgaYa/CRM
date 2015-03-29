c99beec6f04454eee6cab506b6c20596db63425a
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-03-29 23:09:25 +0300

    try №2

--- db/migrate/20150327063225_create_statuses.rb
+++ db/migrate/20150327063225_create_statuses.rb
@@ -12,7 +12,7 @@ class CreateStatuses < ActiveRecord::Migration
 
   def save_status
     status_names = Task.all.pluck(:status)
-    status_names.uniq{|name| Status.new(name: name).save }
+    status_names.each{|name| Status.new(name: name).save }
     Task.all.each do |task|
       status_id = Status.find_by(name: task.status).id
       task.update_attribute(:status_id, status_id)
