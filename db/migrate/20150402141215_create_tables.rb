7ca628695871a163e1d1d5d93e9e9c68f4f28c8d
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-04-02 17:19:50 +0300

    fix

--- db/migrate/20150402141215_create_tables.rb
+++ db/migrate/20150402141215_create_tables.rb
@@ -1,6 +1,19 @@
 class CreateTables < ActiveRecord::Migration
   def change
     create_table :tables do |t|
+      t.string :name, default: :missing, unique: true
+      t.string :level, default: :missing
+      t.string :specialization, default: :missing
+      t.string :email, default: :missing
+      t.string :source, default: :missing
+      t.string :date, default: :missing
+      t.string :comment, default: :missing
+      t.string :status, default: :missing
+      t.string :topic, default: :missing
+      t.string :skype, default: :missing
+      t.string :message, default: :missing
+      t.string :link, default: :missing
+      t.string :assign_to, default: :missing
 
       t.timestamps null: false
     end
