746714cf94135a96a312c79c8fbf624bf38150d7
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-04-02 17:14:01 +0300

    some changing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,23 @@
+# This file is called comments_controller.rb
+# It contains the class CommentsController
 class CommentsController < ApplicationController
-
   include ApplicationHelper
 
   def create
     task = Task.find(params[:task_id])
     time = DateTime.now
-    comment = task.comments.create(user_id: current_user.id , body: params[:body], datetime: time) 
-    task.update_attribute(:date, Date.current())
+    comment = task.comments.create(user_id: current_user.id,
+                                   body: params[:body],
+                                   datetime: time)
+    ubdate_task_date task
     render html: generate_comment(comment, time).html_safe
   end
 
   def destroy
     Comment.find(params[:id]).destroy
+  end
+
+  private def ubdate_task_date(task)
+    task.update_attribute(:date, Date.current)
   end
 end
