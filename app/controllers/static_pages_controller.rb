aeba3a4d374e4f993f1d037f71d254ead381b0a7
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-04-07 09:48:06 +0300

    removed export end added redirect to tables

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
 	def home
-		# redirect_to tasks_path if (current_user && (current_user.status != 'lock'))
+    if current_user
+      if current_user.admin? || current_user.role == 'seller'
+  		  redirect_to tables_path(type: 'SALE')
+      else
+        redirect_to tables_path(type: 'CANDIDATE')
+      end
+    end
   end
 
   def baned_user
