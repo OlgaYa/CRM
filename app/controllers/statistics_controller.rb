ac1b056c9dbd526bbdee3b5b022003e9c8084b28
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-04-29 09:38:22 +0300

    Refactoring controller statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,33 +2,34 @@ class StatisticsController < ApplicationController
   respond_to :json
 
   def index
+    @type = params[:type]
   end
 
   def change_information
     @hash = []
     date_from = params[:dateFrom].to_date.at_beginning_of_week
     date_to = params[:dateTo].to_date.at_end_of_week
-    allusers, allstatus, allsources = [:users, :status, :sources].collect{|k| params[k].blank? ? [""] : params[k]}
+    allusers, allstatus, allsources, alllevels, allspecializations  = [:user, :status, :source, :level, :specialization].collect{|k| params[k].blank? ? [""] : params[k]}
 
-    allsources.each do |x|
-        allusers.each do |y|
-          allstatus.each do |z|
-            name = y.empty? ? "" : "#{User.find_by_id(y).first_name} #{User.find_by_id(y).last_name}"
-            select_user = y.empty? ? "" : "user_id = #{y}"
-            if name.empty?
-              name = x.empty? ? "" : Source.find_by_id(x).name
-            else
-              name += x.empty? ? "" : ": #{Source.find_by_id(x).name}"
+    allsources.each do |source|
+        allusers.each do |user|
+          allstatus.each do |status|
+            alllevels.each do |level|
+              allspecializations.each do |specialization|
+                name = user.empty? ? "" : User.find_by_id(user).full_name
+                select_user = user.empty? ? "" : "user_id = #{user}"
+                name = find_name(name, "source", source)
+                select_source = source.empty? ? "" : "source_id = #{source}"
+                name = find_name(name, "level", level)
+                select_level = level.empty? ? "" : "level_id = #{level}"
+                name = find_name(name, "specialization", specialization)
+                select_specialization = specialization.empty? ? "" : "specialization_id = #{specialization}"
+                name = find_name(name, "status", status)
+                select_status = status.empty? ? "status_id = #{Status.default_status(params[:type])}" : "status_id = #{status}"
+                name = "All" if name.empty?
+                @hash << get_information(name, select_source, select_user, select_status, select_level, select_specialization, date_from, date_to)
+              end
             end
-            select_source = x.empty? ? "" : "source_id = #{x}"
-            if name.empty?
-              name = z.empty? ? "" : Status.find_by_id(z).name
-            else
-              name += z.empty? ? "" : ": #{Status.find_by_id(z).name}"
-            end
-            select_status = z.empty? ? "" : "status_id = #{z}"
-            name = "All" if name.empty?
-            @hash << get_information(name, select_source, select_user, select_status, date_from, date_to)
           end
         end
       end
@@ -42,20 +43,28 @@ class StatisticsController < ApplicationController
     end
   end
 
-  def get_information(name, source, users, status, date_from, date_to)
+  def get_information(name, source, users, status, level, specialization, date_from, date_to)
     inform = {}
     inform[:name] = name
-    inform[:data] = find_in_database(source, users, status, date_from, date_to)
+    inform[:data] = find_in_database(source, users, status, level, specialization, date_from, date_to)
     inform
   end
 
-  def find_in_database(source, users, status, date_from, date_to)
-    inform = Statistic.where(source).where(users).where(status).where(:week => date_from..date_to).group(:week).order(:week).sum(:count)
+  def find_in_database(source, users, status, level, specialization, date_from, date_to)
+    inform = Statistic.where(source).where(users).where(status).where(level).where(specialization).where(:week => date_from..date_to).group(:week).order(:week).sum(:count)
     current_week = date_from
     while current_week < date_to
       inform[current_week] = 0 unless inform.include? (current_week)
       current_week += 1.weeks
     end
     inform.sort.to_a
+  end
+
+  def find_name(name, name_class, id)
+    name += if name.empty?
+      id.empty? ? "" : name_class.singularize.capitalize.constantize.find_by_id(id).name
+    else
+      id.empty? ? "" : ": #{name_class.singularize.capitalize.constantize.find_by_id(id).name}"
+    end
   end
 end
