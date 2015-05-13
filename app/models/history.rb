d7a734b383c9be7f058ec66d2bcae1fc83a18ae8
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-05-13 13:30:01 +0300

    Change logic in model History

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,23 +1,25 @@
 class History < ActiveRecord::Base
-  has_many 		:options_for_history, dependent: :destroy
-  belongs_to 	:table
+  has_many    :options_for_history, dependent: :destroy
+  belongs_to  :table
   belongs_to  :user
 
   def self.create_history_for_new_object(object)
-  	history = History.new(table_id: object.id,
-  												user_id:  object.user_id,
-  												for_type: object.type)
-  	history.save
+    history = History.new(table_id: object.id,
+                          user_id:  object.user_id,
+                          for_type: object.type)
+    history.save
+    # binding.pry
   end
 
   def self.create_history_for_update_object(object, params)
-  	history = History.new(table_id: object.id,
-  												user_id:  object.user_id,
-  												for_type: object.type)
-  	history.save
-  	option = OptionsForHistory.new(history_id: 					history.id,
-  																 history_option_type: params.keys.first.gsub("_id", "").capitalize,
-  																 history_option_id: 	params.values.first.to_i)
-  	option.save
+    history = History.new(table_id:    object.id,
+                          user_id:     object.user_id,
+                          for_type:    object.type,
+                          description: params.values.first)
+    history.save
+    option = OptionsForHistory.new(history_id:          history.id,
+                                   history_option_type: params.keys.first.gsub("_id", "").capitalize,
+                                   history_option_id:   params.values.first.to_i)
+    option.save
   end
 end
--- db/migrate/20150507114452_create_history.rb
+++ db/migrate/20150507114452_create_history.rb
@@ -1,11 +1,12 @@
 class CreateHistory < ActiveRecord::Migration
   def change
     create_table :histories do |t|
-    	t.integer  :table_id
-    	t.integer  :user_id
-    	t.string	 :for_type, default: 'sale'
+      t.integer  :table_id
+      t.integer  :user_id
+      t.string   :for_type, default: 'sale'
+      t.string   :description
 
-    	t.timestamps null: false
+      t.timestamps null: false
     end
   end
 end
