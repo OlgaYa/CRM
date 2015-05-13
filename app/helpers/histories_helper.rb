430b2511db7d4dcc062644a096e96231ea93a134
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-05-13 12:42:31 +0300

    Add relations with histoty_option to models

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -1,21 +1,38 @@
 module HistoriesHelper
-  NOT_MODAL = ["Name", "Lead"]
+  NOT_MODEL = ["Name", "Lead", "Email", "Skype", "Topic"]
 
-  def information_about_change(options)
+  def information_about_change(history)
+    options = history.options_for_history.first
     if options
       key = options.history_option_type
-      value = if NOT_MODAL.include?(key)
-                options.history_option_id
+      value = if NOT_MODEL.include?(key)
+                history.description
               else
-                key == "Name"? options.history_option.full_name : options.history_option.name 
+                case key
+                when "Name"
+                  options.history_option.full_name
+                when "Comment"
+                  options.history_option.body
+                when "Link"
+                  options.history_option.href
+                else
+                  options.history_option.name
+                end
               end
-      "Change #{key} to #{value}"
+      "#{key}: #{value}"
     else
       "Create a new task"
     end
   end
 
   def change_date(history)
-    history.created_at.to_date == @current_day? "today" : history.created_at.to_date
+    if @current_day != history.created_at.to_date
+      @current_day = history.created_at.to_date
+      if @current_day == Date.today
+        "Today"
+      else
+        @current_day
+      end
+    end
   end
 end
