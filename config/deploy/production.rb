1e8d414d56100c8c529bce643979d5a655dc3f72
ZavyalovAndrey <zavyalov79@ukr.net>
2015-05-19 16:10:02 +0300

    add dtaging server in deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,77 +1,11 @@
-# # Simple Role Syntax
-# # ==================
-# # Supports bulk-adding hosts to roles, the primary server in each group
-# # is considered to be the first unless any hosts have the primary
-# # property set.  Don't declare `role :all`, it's a meta role.
+set :branch, "master"
+set :user, "crmuser"
+set :domain, "crmuser@server.sloboda-studio.com"
+set :deploy_to, "/home/crmuser/crmuser_app/"
+set :port, 3333
 
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
-# role :db,  %w{deploy@example.com}
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-
-# # Extended Server Syntax
-# # ======================
-# # This can be used to drop a more detailed server definition into the
-# # server list. The second argument is a, or duck-types, Hash and is
-# # used to set extended properties on the server.
-# server '178.165.91.196', user: 'crmuser', roles: %w{web app db}
-
-
-# # Custom SSH Options
-# # ==================
-# # You may pass any option but keep in mind that net/ssh understands a
-# # limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-# #
-# # Global options
-# # --------------
-# #  set :ssh_options, {
-# #    keys: %w(/home/rlisowski/.ssh/id_rsa),
-# #    forward_agent: false,
-# #    auth_methods: %w(password)
-# #  }
-# #
-# # And/or per server (overrides global)
-# # ------------------------------------
-# # server 'example.com',
-# #   user: 'user_name',
-# #   roles: %w{web app},
-# #   ssh_options: {
-# #     user: 'user_name', # overrides user setting above
-# #     keys: %w(/home/user_name/.ssh/id_rsa),
-# #     forward_agent: false,
-# #     auth_methods: %w(publickey password)
-# #     # password: 'please use keys'
-# #   }
-# set :branch, "master"
-
-# set :rvm_type, :system    # :user is the default
-# set :rvm_ruby_string, '2.1@crm'
-
-# role :web, "178.165.91.196"   
-# role :app, "178.165.91.196" 
-# role :db, "178.165.91.196", primary: true # This is where Rails migrations will run
-
-# set :deploy_to, "/home/crmuser/crmuser_app/"
-
-# namespace :deploy do
-#   remote_file_exists = lambda {|fn| capture("if [ -e #{fn} ]; then echo 1; fi") =~ /1/ }
-
-#   desc "Start the application server"
-#   task :start, roles: :app do
-#   end
-
-#   desc "Stop the application server"
-#   task :stop, roles: :app do
-#     run "kill -s QUIT `cat #{pid}`"
-#   end
-
-#   desc "Restart the application server"
-#   task :restart, roles: :app do
-#     if remote_file_exists.call(pid)
-#       deploy.stop
-#       deploy.start
-#     else
-#       deploy.start
-#     end
-#   end
-# end
+default_run_options[:shell] = '/bin/bash'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,36 +1,11 @@
-set :branch, "master"
-set :rvm_type, :system
-set :rvm_ruby_string, 'ruby-2.0.0-p576@crm_staging'
-set :rvm_ruby_version, '2.0.0-p576'
-set :user, "crmuser"
+set :branch, "staging"
+set :user, "deployer"
+set :domain, "deployer@lxc.sloboda-studio.com"
+set :deploy_to, "/home/deployer/crmuser_app/"
+set :port, 25002
 
-set :domain, "crmuser@server.sloboda-studio.com"
-set :deploy_to, "/home/crmuser/crmuser_app/"
-set :port, 3333
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
 
-namespace :deploy do
-  remote_file_exists = lambda {|fn| capture("if [ -e #{fn} ]; then echo 1; fi") =~ /1/ }
-
-  desc "Start the application server"
-  task :start, roles: :app do
-    run "ln -nfs #{shared_path}/config/unicorn_staging.rb #{release_path}/config/unicorn.rb"
-  end
-
-  desc "Stop the application server"
-  task :stop, roles: :app do
-    run "kill -s QUIT `cat #{pid}`"
-  end
-
-  desc "Restart the application server"
-  task :restart, roles: :app do
-    if remote_file_exists.call(pid)
-      deploy.stop
-      deploy.start
-    else
-      deploy.start
-    end
-  end
-end
+default_run_options[:shell] = '/bin/bash -l'
