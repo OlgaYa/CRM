95a834b29bffc3288ad07f1f2e48696d4a57bb1a
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-05-27 16:49:31 +0300

    Change in db_schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522094944) do
+ActiveRecord::Schema.define(version: 20150525085549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,20 @@ ActiveRecord::Schema.define(version: 20150522094944) do
     t.integer "count",      default: 0
     t.integer "percentage", default: 0
   end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects_users", id: false, force: :cascade do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
+
+  add_index "projects_users", ["project_id"], name: "index_projects_users_on_project_id", using: :btree
+  add_index "projects_users", ["user_id"], name: "index_projects_users_on_user_id", using: :btree
 
   create_table "reports", force: :cascade do |t|
     t.string   "project"
