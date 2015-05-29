b03eb8212a10fd12c182c781f525150d1e4a458f
Yaroslav <Yaroslav.Zhabko@gmail.com>
2015-05-29 15:33:49 +0300

    removed go button

--- app/assets/javascripts/summary_reports.js
+++ app/assets/javascripts/summary_reports.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('.refresh-dt-data').notification('Desktime',
                                      'Data will be updated within a few minutes.',
                                      'click');
+  $('.select-year-and-month').on('change',function(){
+    this.submit();
+  })
 });
