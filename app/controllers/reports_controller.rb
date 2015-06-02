c4c58d655f481ea5ac31642fccdf8bbd180fa7f2
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-06-02 16:25:28 +0300

    Add methods to controller reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,10 +2,14 @@ class ReportsController < ApplicationController
 
   def index
     @date = params[:date_report] ? Date.strptime(params[:date_report], "%m/%Y") : Date.today
-
     @q = Report.all_in_this_month(@date, params[:q], current_user)
     @reports = @q.result.order('date DESC')
+  end
+
+  def new
     @report = Report.new
+    @user = current_user
+    render layout: false
   end
 
   def create
@@ -17,6 +21,7 @@ class ReportsController < ApplicationController
 
   def edit
     @report = Report.find_by_id(params[:id])
+    @user = current_user
     render layout: false
   end
 
@@ -29,9 +34,22 @@ class ReportsController < ApplicationController
   def delete
   end
 
+  def reports_settings
+    @settings = {}
+    @settings[:visible] = Project.all_exsept_project_current_user (current_user)
+    @settings[:invisible] = Project.all_project_current_user (current_user)
+    render json: @settings.to_json
+  end
+
+  def update_report_settings
+    array = params[:invisible].collect{|p| Project.find(p)}
+    current_user.projects = array
+    redirect_to action: :index
+  end
+
   private
 
     def report_params
-      params.require(:report).permit(:hours, :date, :project, :task)
+      params.require(:report).permit(:hours, :date, :project_id, :task)
     end
 end
