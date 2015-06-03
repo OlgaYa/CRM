365ac10348a08776ef0d43703c11a72e2d9ff95e
Anastasiya <anastasiya.cergeevna@yandex.ru>
2015-06-03 13:22:35 +0300

    Add quick search user in the appointment of the project

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -9,14 +9,33 @@ $.fn.listVars = function(){
   return result;
 }
 
+function listFilter(header, list) {
+      var input = $("input#filterinput");
+
+      $(input).change( function () {
+        var filter = $(this).val();
+        if(filter) {
+          $(list).find("p:not(:Contains(" + filter + "))").parent().slideUp();
+          $(list).find("p:Contains(" + filter + ")").parent().slideDown();
+        } else {
+          $(list).find("li").slideDown();
+        }
+        return false;
+      })
+      .keyup( function () {
+        $(this).change();
+      });
+    }
+
+
 function appendNotAssignFields($selector){
   for(var i = 0; i < settings.not_assign.length; i = i+1){
-    $selector.append("<li id='"+settings.not_assign[i][1] + "'>" + settings.not_assign[i][0] +"</li>");
+    $selector.append("<li id='"+settings.not_assign[i][1] + "'> <p>" + settings.not_assign[i][0] +"</p> </li>");
   }
 }
 
 function appendAssignFields($selector){
   for(var i = 0; i < settings.assign.length; i = i+1){
-    $selector.append("<li id='" + settings.assign[i][1] + "'>"+ settings.assign[i][0] +'</li>');
+    $selector.append("<li id='" + settings.assign[i][1] + "'> <p>"+ settings.assign[i][0] +'</p></li>');
   }
 }
