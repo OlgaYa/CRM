db0a58ce082615d6c07b3ab07eaa0372fbc8eaa3
ZavyalovAndrey <zavyalov79@ukr.net>
2015-06-04 14:22:21 +0300

    revert production.rb4; move password section into file smtp.yml

---
+++ config/environments/production.rb
@@ -0,0 +1,24 @@
+CRMProject::Application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = YAML.load_file("#{Rails.root.to_s}/config/smtp.yml")
+  config.action_mailer.default_url_options = { host: 'crm.sloboda-studio.com' }
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  config.log_level = :debug
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.active_job.queue_adapter = :sidekiq
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
+  config.time_zone = 'Europe/Kiev'
+  config.assets.precompile << "*.js"
+end
