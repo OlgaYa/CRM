2c96ee895c8567a02b2209f181219ed5524e7fca
ZavyalovAndrey <zavyalov79@ukr.net>
2015-06-05 16:16:14 +0300

    #3722 fix migrate

--- db/migrate/20150604131029_change_columns_project.rb
+++ db/migrate/20150604131029_change_columns_project.rb
@@ -1,5 +1,7 @@
 class ChangeColumnsProject < ActiveRecord::Migration
   def up
+  	Project.connection.execute("update projects set status='0'")
+  	Project.connection.execute("update projects set kind='0'")
   	change_column :projects, :status, 'integer USING CAST(status AS integer)', default: 0
   	change_column :projects, :kind, 'integer USING CAST(kind AS integer)', default: 0
   end
